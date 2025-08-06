69490cb38fd58e58dbd869819e04dd9d7e51590b
DM <dommcneill@outlook.com>
2025-08-06 21:27:45 +0100

    camera vfx
    + error handling

--- Scripts/TriggerCameraSwitch.cs
+++ Scripts/TriggerCameraSwitch.cs
@@ -3,31 +3,36 @@ using System;
 
 public partial class TriggerCameraSwitch : Area3D
 {
-    [Export]
-    private Camera3D targetCameraSwitch;
+	[Export]
+	private Camera3D targetCameraSwitch;
 
-    private bool hasTriggered = false;
+	private bool hasTriggered = false;
 
-    public override void _Process(double delta)
-    {
-        if (HasOverlappingBodies())
-        {
-            Godot.Collections.Array<Node3D> bodies = GetOverlappingBodies();
+	public override void _Process(double delta)
+	{
+		if (HasOverlappingBodies())
+		{
+			Godot.Collections.Array<Node3D> bodies = GetOverlappingBodies();
 
-            foreach (Node3D node in bodies)
-            {
-                if (node.Name == new StringName("PlayerCharacter") && !hasTriggered)
-                {
-                    hasTriggered = true;
-
-                    GetViewport().GetCamera3D().ClearCurrent(false);
-                    targetCameraSwitch.MakeCurrent();
-                }
-            }
-        }
-        else
-        {
-            hasTriggered = false;
-        }
-    }
+			foreach (Node3D node in bodies)
+			{
+				if (node.Name == new StringName("PlayerCharacter") && !hasTriggered)
+				{
+					hasTriggered = true;
+					
+					if (targetCameraSwitch == null){
+						GD.PrintErr("Cannot switch with a null cam!");
+						return;
+					}
+					
+					GetViewport().GetCamera3D().ClearCurrent(false);
+					targetCameraSwitch.MakeCurrent();
+				}
+			}
+		}
+		else
+		{
+			hasTriggered = false;
+		}
+	}
 }
